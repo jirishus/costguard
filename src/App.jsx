938a84664bd5378f00d9711faa4eea27da9adf8e
jirishus <jirishus@yahoo.com>
2025-02-27 00:33:55 -0800

    fixed header import and js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <h1>CostGuard</h1>
       <p>Easily Track Your Expenses</p>
     </div>
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <p>header content</p>
+    </div>
+  );
+}
+
+export default Header;
