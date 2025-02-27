c4097904cf26acd51fb2e7ffed5c35cf9b580cf2
jirishus <jirishus@yahoo.com>
2025-02-27 00:17:14 -0800

    babelrc webpack config and react source files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CostGuard</h1>
+      <p>Easily Track Your Expenses</p>
+    </div>
+  );
+};
+
+export default App;
